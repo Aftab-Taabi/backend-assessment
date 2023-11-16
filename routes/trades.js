de7382a8dbad3deeb5728d7a7dd1092216d3681d
Aftab-Taabi <hpcontact6730@gmail.com>
2023-11-16 13:44:54 +0500

    code refactored

--- routes/trades.js
+++ routes/trades.js
@@ -1,87 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const Trade = require("../models/trades");
+const {postTrade,getTrades,getTradeById,operationForbidden} = require("../controllers/trades");
 
 // POST endpoint for creating a new trade
-router.post("/", async (req, res) => {
-  try {
-    // Extract the trade object from the request body
-    const { type, user_id, symbol, shares, price, timestamp } = req.body;
-
-    // Create a new trade in the database
-    const newTrade = await Trade.create({
-      type,
-      user_id,
-      symbol,
-      shares,
-      price,
-      timestamp,
-    });
-
-    // Respond with the created trade object
-    res.status(201).json(newTrade);
-  } catch (error) {
-    console.error("Error creating trade:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.post("/", postTrade);
 
 // GET endpoint
-router.get("/", async (req, res) => {
-  try {
-    // Build the query based on the optional parameters
-    const whereClause = {};
-    if (req.query.type) {
-      whereClause.type = req.query.type;
-    }
-    if (req.query.user_id) {
-      whereClause.user_id = parseInt(req.query.user_id);
-    }
-
-    // Fetch trades from Sequelize based on the query
-    const trades = await Trade.findAll({
-      where: whereClause,
-      order: [["id", "ASC"]],
-    });
-
-    // Respond with the array of trades
-    res.status(200).json(trades);
-  } catch (error) {
-    console.error("Error fetching trades:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.get("/", getTrades);
 
 router
   .route("/:id")
   // GET endpoint for retrieving a trade by ID
-  .get(async (req, res) => {
-    try {
-      // Extract the trade ID from the request parameters
-      const tradeId = parseInt(req.params.id);
-
-      // Find the trade in the database by ID
-      const trade = await Trade.findByPk(tradeId);
-
-      // Respond based on whether the trade is found or not
-      if (trade) {
-        res.status(200).json(trade);
-      } else {
-        res.status(404).send("ID not found");
-      }
-    } catch (error) {
-      console.error("Error retrieving trade:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  })
-  .put((req, res) => {
-    res.status(405).send("Operation Not Allowed");
-  })
-  .patch((req, res) => {
-    res.status(405).send("Operation Not Allowed");
-  })
-  .delete((req, res) => {
-    res.status(405).send("Operation Not Allowed");
-  });
+  .get(getTradeById)
+  .put(operationForbidden)
+  .patch(operationForbidden)
+  .delete(operationForbidden);
 
 module.exports = router;
